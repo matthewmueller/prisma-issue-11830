f4ef6030db85e524b3bb83d7618d6248fa5bad5d
Matthew Mueller <mattmuelle@gmail.com>
2022-02-16 15:25:17 -0600

    add instructions and things I tried

---
+++ Readme.md
@@ -0,0 +1,9 @@
+Reproduction for: https://github.com/prisma/prisma/issues/11830
+
+```
+git clone https://github.com/matthewmueller/prisma-issue-11830
+cd prisma-issue-11830
+npm install
+# Edit .env (ask me for creds)
+npx ts-node index.ts
+```
--- index.ts
+++ index.ts
@@ -8,8 +8,24 @@ async function main() {
     filter: {
       name: "Fashion",
     },
+
+    // One of the below should work, probably the _id since this is a raw
+    // query that maps to the underlying _id:
+
     // filter: {
-    //   _id: { $eq: new ObjectId("61e20bbd7fef3b66f439373c") },
+    //   _id: { $eq: new ObjectId("61e20bbd7fef3b66f439373b") },
+    // } as any,
+
+    // filter: {
+    //   _id: { $eq: "61e20bbd7fef3b66f439373b" },
+    // } as any,
+
+    // filter: {
+    //   id: { $eq: new ObjectId("61e20bbd7fef3b66f439373b") },
+    // } as any,
+
+    // filter: {
+    //   id: { $eq: "61e20bbd7fef3b66f439373b" },
     // } as any,
   })
   console.log(categories)
